e45262ecb42d25f85934dc3c76a330f9bbc2d811
hhhsdi <tomasklodettmorell@gmail.com>
2025-03-06 10:21:33 +0300

    Update main.cpp

--- oop/oop/main.cpp
+++ oop/oop/main.cpp
@@ -3,11 +3,25 @@
 #include "SingleStorageBackup.h"
 #include "BackupJob.h"
 #include <memory>
+#include <fstream>
+
+void CreateFile(const std::string& fileName, const std::string& content) {
+    std::ofstream file(fileName);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+    }
+}
 
 int main() {
+    setlocale(LC_ALL, "rus");
     FileStorage storage;
     SplitStorageBackup splitStrategy;
     SingleStorageBackup singleStrategy;
+
+    CreateFile("file1.txt", "1121341");
+    CreateFile("file2.txt", "321321");
+
 
     BackupJob job(singleStrategy, storage);
     auto file1 = std::make_shared<BackupObject>("file1.txt");
@@ -19,3 +33,4 @@ int main() {
 
     return 0;
 }
+
